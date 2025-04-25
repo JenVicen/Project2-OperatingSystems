eaec1067c6febcb4f721a9ba470fed89e5751859
Jen <jenvicen@ttu.edu>
2025-04-24 23:10:42 -0600

    improving code

--- car-rental-simulator/src/main/java/carrental/RentalShop.java
+++ car-rental-simulator/src/main/java/carrental/RentalShop.java
@@ -17,6 +17,8 @@ import java.io.*;
 import java.nio.channels.FileChannel;
 import java.nio.channels.FileLock;
 import java.util.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 public class RentalShop {
     // Shop state fields
